21ced0452a34805e4ec73d88e831742d1bbddd04
Esteban Mercado <esteban.mercadom99@gmail.com>
2026-01-06 20:28:07 -0600

    Se agrega clase Grupo con la capacidad de añadir integrantes en un array tipo player[C

--- task6.js
+++ task6.js
@@ -1,7 +1,76 @@
 export class Player {
-    constructor() {
-      
-    }
-  
-    
+
+  name;
+  level;
+  currentExp;
+  expThreshold;
+
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
+    this.currentExp = 0;
+    this.expThreshold = 10; // límite de experiencia del primer nivel
   }
+
+  get name(){
+    return this.name;
+  }
+
+  set name(value){
+    this.name = value;
+  }
+
+  get level(){
+    return this.level;
+  }
+
+  set level(value){
+    this.level = value;
+  }
+
+  get currentExp(){
+    return this.currentExp;
+  }
+
+  set currentExp(value){
+    this.currentExp = value;
+  }
+
+
+  info(){
+    return `${this.name} has reached Level ${this.level}! and has ${this.currentExp} / ${this.expThreshold} exp points`
+  }
+
+  levelUp(){
+    this.level += 1
+    this.expThreshold = Math.floor(this.expThreshold * 1.1)
+  }
+
+  addExp(value){ // funcióon recursiva por si sube muchos puntos de experiencia y supera más de un nivel a la vez
+    console.log(`${this.name} gained ${value} exp point(s)`)
+    while(value + this.currentExp >= this.expThreshold){
+      value -= this.expThreshold
+      this.levelUp()
+      // console.log(`Subiste de nivel, todavía acumulas ${value} estos puntos de exp para seguir subiendo de nivel`)
+    }
+    this.currentExp += value
+  }
+}
+
+class Grupo {
+  integrantes = [];
+
+  addPlayer(Player){
+    this.integrantes.push(Player)
+  }
+}
+
+
+const esteban = new Player("Esteban", 1)
+const Jose = new Player("Jose", 2)
+
+const grupo1 = new Grupo()
+grupo1.addPlayer(esteban)
+grupo1.addPlayer(Jose)
+
+console.log(grupo1)
