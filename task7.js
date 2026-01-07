c06c8699f26b77801d606bdab9867ec64d88570a
Esteban Mercado <esteban.mercadom99@gmail.com>
2026-01-06 22:16:27 -0600

    Se agrega un objeto como parámetro en clase Player y método para agregar objetos y cantidad

--- task7.js
+++ task7.js
@@ -1,7 +1,77 @@
 export class Player {
-    constructor() {
-      
-    }
-  
-    
+
+  name;
+  level;
+  currentExp;
+  expThreshold;
+  inventory = {};
+
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
+    this.currentExp = 0;
+    this.expThreshold = 10; // límite de experiencia del primer nivel
   }
+
+  get name(){
+    return this.name;
+  }
+
+  set name(value){
+    this.name = value;
+  }
+
+  get level(){
+    return this.level;
+  }
+
+  set level(value){
+    this.level = value;
+  }
+
+  get currentExp(){
+    return this.currentExp;
+  }
+
+  set currentExp(value){
+    this.currentExp = value;
+  }
+
+
+  info(){
+    return `${this.name} has reached Level ${this.level}! and has ${this.currentExp} / ${this.expThreshold} exp points`
+  }
+
+  levelUp(){
+    this.level += 1
+    this.expThreshold = Math.floor(this.expThreshold * 1.1)
+  }
+
+  addExp(value){ // funcióon recursiva por si sube muchos puntos de experiencia y supera más de un nivel a la vez
+    console.log(`${this.name} gained ${value} exp point(s)`)
+    while(value + this.currentExp >= this.expThreshold){
+      value -= this.expThreshold
+      this.levelUp()
+      // console.log(`Subiste de nivel, todavía acumulas ${value} estos puntos de exp para seguir subiendo de nivel`)
+    }
+    this.currentExp += value
+  }
+
+  addObj(key, qty){
+    if(key in this.inventory)
+      this.inventory[key] += qty
+    else
+      this.inventory[key] = qty
+  }
+}
+
+const esteban = new Player("Esteban", 1)
+
+esteban.addObj("grenades", 5)
+console.log(esteban)
+
+esteban.addObj("grenades", 5)
+console.log(esteban)
+
+esteban.addObj("ammo", 5)
+console.log(esteban)
